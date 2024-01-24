e7208a95b807a2f5425ff8e1faeb0a947207cf44
df <dferguson@gmail.com>
2024-01-24 18:15:47 +0800

    add j+k shortcuts

--- .config/nvim/lua/djfergus/remap.lua
+++ .config/nvim/lua/djfergus/remap.lua
@@ -19,3 +19,10 @@ vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 
 vim.keymap.set("n", "Q", "<nop>")
+
+-- mash j*k to exit i mode
+local options = { noremap = true }
+vim.keymap.set("i", "jj", "<Esc>", options)
+vim.keymap.set("i", "kk", "<Esc>", options)
+vim.keymap.set("i", "jk", "<Esc>", options)
+vim.keymap.set("i", "kj", "<Esc>", options)
